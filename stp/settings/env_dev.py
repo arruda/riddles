e733d322e5c699cd0a7765ebbde1a3f9f575f29d
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2013-06-15 23:52:08 -0300

    changing dev settings to use pg fix #7

--- stp/settings/env_dev.py
+++ stp/settings/env_dev.py
@@ -1,16 +1,15 @@
-from utils import LOCAL
 SITE_ID = 1
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 SERVE_MEDIA = True
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': LOCAL('db.sqlite') ,
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'stpdb' ,
+        'USER': 'stp_u',
+        'PASSWORD': 'stp_u',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
