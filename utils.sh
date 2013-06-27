1d1185aedd85384016b22969553c4b07b8d3c444
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2013-06-27 20:28:15 -0300

    adding i18n utils script refs #10

---
+++ utils.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+COMANDO=$1
+
+i18n(){
+    echo "atualizando .mo"
+    cd riddles
+    django-admin.py makemessages -a
+    django-admin.py compilemessages
+    cd -
+}
+
+i18n_criar(){
+    echo "atualizando .mo"
+    cd riddles
+    django-admin.py makemessages -l pt_BR
+    cd -
+}
+
+$COMANDO
