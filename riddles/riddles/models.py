7c561bad4c1c82dc92f2a148aa9e6cb5ffe36576
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2013-06-27 22:06:48 -0300

    doing a simple model

--- riddles/riddles/models.py
+++ riddles/riddles/models.py
@@ -9,11 +9,18 @@
 """
 
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
 
 class Riddle(models.Model):
     """
     Riddle model
 	"""
+    content = models.CharField(_("Content"),max_length=250)
+    answer = models.CharField(_("Answer"),max_length=250)
 
     class Meta:
         app_label = 'riddles'
+
+    def __unicode__(self):
+        return self.content
