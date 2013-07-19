6d754930d3866b500ea6d562b3ad15273d9069fa
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2013-07-19 15:20:09 -0300

    rm deprecated calls for urls

--- riddles/urls.py
+++ riddles/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic import TemplateView
 
@@ -10,20 +10,20 @@ from django.conf import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       
+
      url(r'^admin/', include(admin.site.urls)),
      url(r'^$', TemplateView.as_view(template_name="index.html"), name='index'),
 )
 
 
-#urlpatterns += patterns('django.contrib.auth.views',               
+#urlpatterns += patterns('django.contrib.auth.views',
 #
-#    url(r'^login/$', 'login', {'template_name': 'users/login.html',}, name='login'),  
+#    url(r'^login/$', 'login', {'template_name': 'users/login.html',}, name='login'),
 #    url(r'^logout/$', 'logout', {'template_name': 'users/login.html'},name='logout'),
-#        
+#
 #
 #)
 
 if settings.SERVE_MEDIA:
-    
+
     urlpatterns += staticfiles_urlpatterns()
